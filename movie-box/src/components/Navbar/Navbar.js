53ed21f4229d50b6263689484eaa11a020334bd6
prajapatilalit <prajapatilalit150992@gmail.com>
2021-10-08 14:00:18 +0530

    update styles

--- movie-box/src/components/Navbar/Navbar.js
+++ movie-box/src/components/Navbar/Navbar.js
@@ -4,14 +4,19 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="nav_container">
-      <ul>
-        <Link to="/">
-          <li>Movies</li>
-        </Link>
-        <Link to="/addMovie">
-          <li>Add Movie</li>
-        </Link>
-      </ul>
+      <div>
+        <i class="fas fa-video 5x"></i>
+      </div>
+      <div>
+        <ul>
+          <Link to="/">
+            <li>Movies</li>
+          </Link>
+          <Link to="/addMovie">
+            <li>Add Movie</li>
+          </Link>
+        </ul>
+      </div>
     </div>
   );
 };
